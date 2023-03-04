ac73774062fcc8a853ff377053940723456084a2
Enryuu <mercado.ricohermoso@gmail.com>
2023-03-04 20:28:22 +0800

    Update replace_line.sh and dummy files.

--- replace_line.sh
+++ replace_line.sh
@@ -5,6 +5,13 @@ INPUT_FILE="list_of_files.txt"
 echo "Please provide string pattern to identify line to replace:"
 read input_string
 
+if [ $input_string == "BATCH_SIM_DIR" ]
+then
+  dir="/user/server01/foundry/tech_node/folder/ip_ckt/simulations/ip/technology-code/release_ver/design/sim_data/tb_block_name/"
+else
+  dir=""
+fi
+
 # Loop through each file listed in the input file
 for FILENAME in $(cat "$INPUT_FILE"); do
   # Check if the file exists
@@ -15,12 +22,15 @@ for FILENAME in $(cat "$INPUT_FILE"); do
   
   #Identify line number of input string
   LINE_NUMBER=$(grep -n -F -w "$input_string" FILENAME | cut -d : -f 1)
+
+  # Count the whitespace characters
+  count_space=$(expr "$input_string" : '^[^[:space:]]*[[:space:]]*\(.*\)') 
   
   # Get the new line from the filename
   NEW_LINE=$(basename "$FILENAME" | cut -d '.' -f 1)
   
   # Replace the line
-  sed -i "${LINE_NUMBER}s/.*/\t\t${NEW_LINE}/" "$FILENAME"
+  sed -i "${LINE_NUMBER}s/.*/$("${input_string}${count_space}${dir}${NEW_LINE}")/" "$FILENAME"
   
   echo "Replaced line in $FILENAME."
 done
